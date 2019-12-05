a5b874cb6233bc782d6e79cc3d73d8ec29e85c80
svonhatten <svonhatten@luc.edu>
2019-12-04 20:34:46 -0600

    Added radio buttons for search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import MyComponent from './components/MyComponent.js'
@@ -15,6 +15,57 @@ function App() {
       <header className="App-header">
         <h1>Ecommerce</h1>
         <MyComponent searchID={2}/>
+        {/* Radio Buttons for Search */}
+        <form className="Radio-buttons">
+          {/* Customer option */}
+          <div>
+            <label>
+              <input 
+                type="radio"
+                name="service-options"
+                value="customer"
+                className="form-check-input"
+              />
+              Customer
+            </label>
+          </div>
+          {/* Order option */}
+          <div>
+            <label>
+              <input 
+                type="radio"
+                name="service-options"
+                value="order"
+                className="form-check-input"
+              />
+              Order
+            </label>
+          </div>
+          {/* Product option */}
+          <div>
+            <label>
+              <input 
+                type="radio"
+                name="service-options"
+                value="product"
+                className="form-check-input"
+              />
+              Product
+            </label>
+          </div>
+          {/* Partner option */}
+          <div>
+            <label>
+              <input 
+                type="radio"
+                name="service-options"
+                value="partner"
+                className="form-check-input"
+              />
+              Partner
+            </label>
+          </div>
+        </form>
       </header>
     </div>
   );
