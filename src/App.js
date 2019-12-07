4992e7eaa3fd9f260dd96b7345bcefbc8f965450
svonhatten <svonhatten@luc.edu>
2019-12-06 20:54:34 -0600

    something small, but added border box around search area

--- src/App.js
+++ src/App.js
@@ -14,58 +14,60 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Ecommerce</h1>
-        <MyComponent searchID={2}/>
-        {/* Radio Buttons for Search */}
-        <form className="Radio-buttons">
-          {/* Customer option */}
-          <div>
-            <label>
-              <input 
-                type="radio"
-                name="service-options"
-                value="customer"
-                className="form-check-input"
-              />
-              Customer
-            </label>
-          </div>
-          {/* Order option */}
-          <div>
-            <label>
-              <input 
-                type="radio"
-                name="service-options"
-                value="order"
-                className="form-check-input"
-              />
-              Order
-            </label>
-          </div>
-          {/* Product option */}
-          <div>
-            <label>
-              <input 
-                type="radio"
-                name="service-options"
-                value="product"
-                className="form-check-input"
-              />
-              Product
-            </label>
-          </div>
-          {/* Partner option */}
-          <div>
-            <label>
-              <input 
-                type="radio"
-                name="service-options"
-                value="partner"
-                className="form-check-input"
-              />
-              Partner
-            </label>
-          </div>
-        </form>
+        <div className="Search-box">
+          <MyComponent searchID={2}/>
+          {/* Radio Buttons for Search */}
+          <form className="Radio-buttons">
+            {/* Customer option */}
+            <div>
+              <label>
+                <input 
+                  type="radio"
+                  name="service-options"
+                  value="customer"
+                  className="form-check-input"
+                />
+                Customer
+              </label>
+            </div>
+            {/* Order option */}
+            <div>
+              <label>
+                <input 
+                  type="radio"
+                  name="service-options"
+                  value="order"
+                  className="form-check-input"
+                />
+                Order
+              </label>
+            </div>
+            {/* Product option */}
+            <div>
+              <label>
+                <input 
+                  type="radio"
+                  name="service-options"
+                  value="product"
+                  className="form-check-input"
+                />
+                Product
+              </label>
+            </div>
+            {/* Partner option */}
+            <div>
+              <label>
+                <input 
+                  type="radio"
+                  name="service-options"
+                  value="partner"
+                  className="form-check-input"
+                />
+                Partner
+              </label>
+            </div>
+          </form>
+        </div>
       </header>
     </div>
   );
